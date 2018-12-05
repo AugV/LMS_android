0a81303d8696170c7fa5fd83dc63a4f179aacee0
AUGUSTINAS VAINIUS <augustinas.vainius@gmail.com>
2018-12-05 13:14:57 +0200

    login page set theoretically

--- app/src/main/java/com/vainius/augustinas/lms_android/login/LoginPageFragment.java
+++ app/src/main/java/com/vainius/augustinas/lms_android/login/LoginPageFragment.java
@@ -1,15 +1,29 @@
 package com.vainius.augustinas.lms_android.login;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import com.vainius.augustinas.lms_android.common.BaseFragment;
 
 public class LoginPageFragment extends BaseFragment implements LoginPageViewMVC.LoginPageViewMVCListener{
 
 
-    
+    LoginPageViewMVC mViewMVC;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        mViewMVC = new LoginPageViewMVCImpl(inflater, container);
+        mViewMVC.setListener(this);
+        return mViewMVC.getRootView();
+    }
+
 
 
     @Override
     public void onLoginButtonClicked(String loginId, String loginPsw) {
-
+            Bundle args = new Bundle(2);
+            args.putString("student_id", loginId);
+            args.putString("student_psw", loginPsw);
     }
 }
